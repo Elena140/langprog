92dd5e71a05e0a7b43fb788f8221d27e85a7f09c
Elena140 <167410560+Elena140@users.noreply.github.com>
2026-02-20 19:50:05 +0700

    Delete _QueueFullException.java

--- _QueueFullException.java
+++
@@ -1,9 +0,0 @@
-public class QueueFullException extends Exception {
-	int size;
-	QueueFullException(int s) {
-		size = s;
-	}
-	public String toString() {
-		return "\nОчередь ВСЁ. Макс. размер: " + size;
-	}
-}
